d913c05b47dd9b2d7f63c18c98e0827d6d60f49e
Priyanka053 <palpriyanka053@gmail.com>
2025-02-01 18:20:08 +0530

    Check1

---
+++ Selenium_Concepts/handling_text_box.py
@@ -0,0 +1,63 @@
+
+
+# str="Priyanka"
+#
+# rev_str= str[::-1]
+# print(rev_str)
+#
+# re_str=reversed(str)
+# print("".join(re_str))
+#
+# rev=""
+# for i in str:
+#     rev=i+rev
+# print(rev)
+
+
+# lis=[1,2,3,4,5,6,7,8,9]
+#
+# target=9
+
+# for target in lis:
+#     if target==9:
+#         print("my target element is found")
+
+# if target in lis:
+#     print("target found")
+
+
+# num=6
+# fact=1
+# for i in range(1, num+1):
+#     fact=fact*i
+# print(fact)
+
+# str="priyanka"
+# vowels="aeiou"
+# new_str=[]
+# for i in str:
+#     if i not in vowels:
+#         new_str.append(i)
+# print(''.join(new_str))
+
+# str="priyanka"
+# repeated_char=set()
+# for i in str:
+#     if i not in repeated_char:
+#         char_count=str.count(i)
+#     # print(char_count)
+#
+#     if char_count > 1:
+#         print(i, char_count)
+#     repeated_char.add(i)
+
+# fib=8
+# a,b=0,1
+# fib_series=[]
+# while len(fib_series) < fib:
+#     fib_series.append(a)
+#     a,b=b,a+b
+# print(fib_series)
+
+
+
